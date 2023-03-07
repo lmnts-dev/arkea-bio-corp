f28efdb989f87a2168090199d2b9388890f5cbb8
Jose Rios <jose@commence.studio>
2023-03-07 13:40:33 -0800

    :construction: Copyright

--- wp-content/themes/commence/functions.php
+++ wp-content/themes/commence/functions.php
@@ -7,3 +7,9 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
 
+function year_shortcode () {
+    $year = date_i18n ('Y');
+    return $year;
+}
+
+add_shortcode ('year', 'year_shortcode');
