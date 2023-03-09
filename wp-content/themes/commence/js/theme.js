a0090c51bf06e5055adf64acfc990054777514e4
Jose Rios <jose@commence.studio>
2023-03-08 22:24:50 -0800

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,8 +1,10 @@
-function mobile_nav () {
-    if(window.scrollY==0){
-        console.log('at top');
-    }
-    else{
-        console.log('not at top');
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".mobile-nav--sticky");
+  if (element.scrollTop == 0) {
+    console.log(nav, 'nav here');
+    nav.classList.toggle('transparent');
+  }
+  else {
+    nav.classList.toggle('transparent');
+  }
+});
