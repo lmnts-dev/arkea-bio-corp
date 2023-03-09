c5e32a3aee2ba7f0109c1224058ab98b2b268f26
Jose Rios <jose@commence.studio>
2023-03-08 23:01:10 -0800

    :construction: Contact us section

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+  const nav = document.querySelector(".mobile-nav--sticky");
+  console.log(nav, 'nav here');
+  if (window.scrollY == 0) {
+    console.log('works');
+    nav.classList.toggle('color');
+  }
 });
