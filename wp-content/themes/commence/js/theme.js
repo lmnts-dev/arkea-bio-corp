0c5601f68e5f65582bb0e13b9fc81be9cc9f5392
Jose Rios <jose@commence.studio>
2023-03-09 18:23:15 -0800

    :bug: Undo transform change

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -3,7 +3,7 @@ let strand2 = document.querySelector("#dna-strand-2");
 
 
  window.addEventListener("scroll", () => {
-   let top = window.pageYOffset / 20;
+   let top = window.pageYOffset / 10;
    strand1.style.transform = 'rotateX(' + top + 'deg)';
    strand2.style.transform = 'rotateX(' + top + 'deg)';
 });
