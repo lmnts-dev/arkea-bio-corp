37d19ce32e64adcab1b9f343462b02408ace4d2a
Jose Rios <jose@commence.studio>
2023-03-09 18:01:49 -0800

    :construction: Dna animation

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
+// let strand1 = document.querySelector("#dna-strand-1");
+
+//  window.addEventListener("scroll", () => {
+//   var top = window.pageYOffset / 10;
+//   strand1.style.transform = 'rotate3d(0.5, 0.5, 0.5, ' + top + 'deg)';
+// });
+// });
+
 let strand1 = document.querySelector("#dna-strand-1");
 
  window.addEventListener("scroll", () => {
   var top = window.pageYOffset / 10;
-  strand1.style.transform = 'rotate3d(0.5, 0.5, 0.5, ' + top + 'deg)';
-});
-});
-
-let strand1 = document.querySelector("#dna-strand-1");
-console(strand1, 'strand 1');
- window.addEventListener("scroll", () => {
-  var top = window.pageYOffset / 10;
-  strand1.style.transform = 'rotate3d(0.5, 0.5, 0.5, ' + top + 'deg)';
+  strand1.style.transform = 'rotate3d(0.5, 0, 0.5, ' + top + 'deg)';
 });
