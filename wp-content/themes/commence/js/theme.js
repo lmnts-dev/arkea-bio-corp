4aa718771a14825d5f767272d5061e02520bc381
Jose Rios <jose@commence.studio>
2023-03-09 18:08:40 -0800

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,15 +1,8 @@
-// document.addEventListener("DOMContentLoaded", function () {
-// let strand1 = document.querySelector("#dna-strand-1");
-
-//  window.addEventListener("scroll", () => {
-//   var top = window.pageYOffset / 10;
-//   strand1.style.transform = 'rotate3d(0.5, 0.5, 0.5, ' + top + 'deg)';
-// });
-// });
-
 let strand1 = document.querySelector("#dna-strand-1");
+let strand2 = document.querySelector("#dna-strand-2");
+
 
  window.addEventListener("scroll", () => {
   var top = window.pageYOffset / 10;
-  strand1.style.transform = 'rotate3d(0.5, 0, 0, ' + top + 'deg)';
+  strand1.style.transform = 'rotateX(' + top + 'deg)';
 });
