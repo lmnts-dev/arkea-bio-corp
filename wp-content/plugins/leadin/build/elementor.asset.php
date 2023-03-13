780bcb6a853718f372d5b4fa549740766f3c5c8e
Jose Rios <jose@commence.studio>
2023-03-13 15:43:57 -0700

    :rocket: Add Hubspot plugin

---
+++ wp-content/plugins/leadin/build/elementor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-i18n'), 'version' => '4d67363ca2dd0b294977');
---
+++ wp-content/plugins/leadin/build/leadin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-i18n'), 'version' => '4a8e86b8ae2b3abfb5a6');
