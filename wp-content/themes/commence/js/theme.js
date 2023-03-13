ebfbb2b38203a6b1d2ab251b65ed0cbdd5688513
Jose Rios <jose@commence.studio>
2023-03-13 11:59:35 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -7,9 +7,14 @@ let spiral = document.querySelector('.spiral img');
 let spiral2 = document.querySelector('.spiral--future img');
 
  window.addEventListener("scroll", () => {
-  let top2 = window.pageYOffset / 2; 
+   let top2 = window.pageYOffset / 2; 
+   let scrollPos = window.scrollY;
   spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
    spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
+   animatedImage.style.animationPlayState = 'running';
    
-  animatedImage.style.animationPlayState = 'running';
+  const translateX = scrollPos * 0.5; // adjust the speed of the slide here
+  
+  strand1.style.transform = `translateX(${translateX}px)`;
+
  });
