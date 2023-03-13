0ec8a1ba0a6a1663d6177e35a3c59617f709a7d8
Jose Rios <jose@commence.studio>
2023-03-13 00:43:43 -0700

    :construction: Re-add js

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,15 +1,15 @@
-// let strand1 = document.querySelector("#dna-strand-1 img");
-// let strand2 = document.querySelector("#dna-strand-2 img");
+let strand1 = document.querySelector("#dna-strand-1 img");
+let strand2 = document.querySelector("#dna-strand-2 img");
 
-// let spiral = document.querySelector('.spiral img');
-// let spiral2 = document.querySelector('.spiral--future img');
+let spiral = document.querySelector('.spiral img');
+let spiral2 = document.querySelector('.spiral--future img');
 
-//  window.addEventListener("scroll", () => {
-//   let top = window.pageYOffset / 4;
-//   let top2 = window.pageYOffset / 2;
-//   strand1.style.transform = 'rotateX(' + top + 'deg)';
-//   strand2.style.transform = 'rotateX(' + top + 'deg)';
+ window.addEventListener("scroll", () => {
+  let top = window.pageYOffset / 4;
+  let top2 = window.pageYOffset / 2;
+  strand1.style.transform = 'rotateX(' + top + 'deg)';
+  strand2.style.transform = 'rotateX(' + top + 'deg)';
    
-//   spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
-//   spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
-// });
+  spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
+  spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
+});
