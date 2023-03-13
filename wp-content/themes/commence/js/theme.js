ab7f8e8f242a21a708b15dee7b865621c01537d7
Jose Rios <jose@commence.studio>
2023-03-12 19:06:30 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,10 +1,13 @@
 let strand1 = document.querySelector("#dna-strand-1 img");
 let strand2 = document.querySelector("#dna-strand-2 img");
 
+let spiral = document.querySelector('#spiral img');
 
  window.addEventListener("scroll", () => {
   let top = window.pageYOffset / 10;
   let top2 = window.pageYOffset / 10;
   strand1.style.transform = 'rotateX(' + top + 'deg)';
   strand2.style.transform = 'rotateX(' + top2 + 'deg)';
+   
+  spiral.style.transform = 'rotateY(' + top + 'deg)';
 });
