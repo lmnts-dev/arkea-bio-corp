db9ef424cee1ed45f0efd857c161cdfe732bd869
Jose Rios <jose@commence.studio>
2023-03-13 01:06:03 -0700

    :construction: Dna strands

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -7,8 +7,7 @@ let spiral2 = document.querySelector('.spiral--future img');
  window.addEventListener("scroll", () => {
   let top = window.pageYOffset / 4;
   let top2 = window.pageYOffset / 2;
-   strand1.style.transform = 'rotateX(' + top + 'deg)';
-   strand1.style.animation = 'moving 5s infinite linear';
+  strand1.style.transform = 'rotateX(' + top + 'deg)';
   strand2.style.transform = 'rotateX(' + top + 'deg)';
    
   spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
