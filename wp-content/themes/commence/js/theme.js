eed49c3bd5046171ca6d4ae610e071d1685bbbf6
Jose Rios <jose@commence.studio>
2023-03-14 17:53:58 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -7,14 +7,3 @@ let spiral2 = document.querySelector('.spiral-2 img');
   spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
  });
 
-
- const container = document.querySelector('.ciclegraph .uc-items-wrapper');
-
-// Get a reference to the active item
-const activeItem = document.querySelector('.uc-circle.uc-circle-selected');
-
-// Calculate the rotation angle needed to move the active item to the top
-const angle = -1 * (360 / container.children.length) * Array.from(container.children).indexOf(activeItem);
-
-// Apply the necessary CSS transforms to rotate the items
-container.style.transform = `rotate(${angle}deg)`;
