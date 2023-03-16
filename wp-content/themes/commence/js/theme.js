d490f5abdfa3779e65b36bea522b8a153e1862d1
Jose Rios <jose@commence.studio>
2023-03-15 23:44:31 -0700

    :construction: Nav

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,10 +1,10 @@
 let spiral = document.querySelector('.spiral img');
-let spiral2 = document.querySelector('.spiral-2 img');
+let spiral2 = document.querySelector('.second-spiral img');
 
  window.addEventListener("scroll", () => {
   let top2 = window.pageYOffset / 2; 
   spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
-  // spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
+  spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
  });
 
 let graphic = document.querySelectorAll('.uc-circle');
