286fea62170188ad3590b7a8f41f4f17cba17e18
Jose Rios <jose@commence.studio>
2023-03-16 18:20:27 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -2,15 +2,13 @@ let spiral = document.querySelector('.spiral-container-2 .second-spiral');
 let spiral2 = document.querySelector('.spiral-container .second-spiral');
 var scrollPosition = window.scrollY;
 let rotateValue = "rotate(" + scrollPosition + "deg)";
-let circle = document.querySelector("#uc_interactive_circle_infographic_elementor24480.ciclegraph");
+let circle = document.querySelector("#uc_interactive_circle_infographic_elementor24480 .circle");
 
  window.addEventListener("scroll", () => {
   let top2 = window.pageYOffset / 2; 
   spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
    spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
-
-   let beforeCircle = window.getComputedStyle(circle, "::before");
-   beforeCircle.transform = rotateValue;
+   circle.style.transform = 'rotateZ(' + top2 + 'deg)';
  });
 
 let graphic = document.querySelectorAll('.uc-circle');
