1c9a2dc55de1e6303fab48b043b0fb89eac2def5
Jose Rios <jose@commence.studio>
2023-03-16 23:05:44 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,4 +1,4 @@
-let spiral = document.querySelector('.spiral-container-2 .second-spiral');
+let spirals2 = document.querySelector('.spiral-container .second-spiral path');
 let spirals = document.querySelectorAll('.spiral-container-2 .second-spiral path');
 var scrollPosition = window.scrollY;
 let rotateValue = "rotate(" + scrollPosition + "deg)";
@@ -9,8 +9,6 @@ window.addEventListener("scroll", () => {
   let top1 = window.pageYOffset / 5; 
   let top2 = window.pageYOffset / 2; 
    
-  // spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
-  spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
      var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
    if (st > lastScrollTop) {
     circle.style.transform = 'rotateZ(' + top2 + 'deg)';
@@ -41,6 +39,7 @@ graphic.forEach(el => el.addEventListener('click', () => {
 }));
 
 let newSpirals = Array.from(spirals);
+let newSpirals2 = Array.from(spirals2);
 
 window.addEventListener("scroll", () => {
   let top3 = window.pageYOffset / 2;
@@ -52,16 +51,32 @@ window.addEventListener("scroll", () => {
     if (idx % 2 === 0) {
       spiral.style.transform = 'rotate(' + top3 + 'deg)';
     }
-    else if (idx % 3 == 0 ) {
+    else if (idx % 3 == 0) {
       spiral.style.transform = 'rotate(' + top4 + 'deg)';
     }
-    else if (idx % 5 == 0 ) {
+    else if (idx % 5 == 0) {
       spiral.style.transform = 'rotate(' + top5 + 'deg)';
     }
     else {
       spiral.style.transform = 'rotate(' + top6 + 'deg)';
     }
-  })
+  });
+
+    newSpirals2.map((spiral, idx) => {
+    if (idx % 2 === 0) {
+      spiral.style.transform = 'rotate(' + top3 + 'deg)';
+    }
+    else if (idx % 3 == 0) {
+      spiral.style.transform = 'rotate(' + top4 + 'deg)';
+    }
+    else if (idx % 5 == 0) {
+      spiral.style.transform = 'rotate(' + top5 + 'deg)';
+    }
+    else {
+      spiral.style.transform = 'rotate(' + top6 + 'deg)';
+    }
+  });
+  
   });
 
 
