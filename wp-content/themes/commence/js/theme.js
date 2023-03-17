1b93fc60fbdd2d0250c67412d89eb4ff7cbc4669
Jose Rios <jose@commence.studio>
2023-03-16 17:59:30 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -1,10 +1,14 @@
 let spiral = document.querySelector('.spiral-container-2 .second-spiral');
 let spiral2 = document.querySelector('.spiral-container .second-spiral');
+var scrollPosition = window.scrollY;
+let rotateValue = "rotate(" + scrollPosition + "deg)";
+
 
  window.addEventListener("scroll", () => {
   let top2 = window.pageYOffset / 2; 
   spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
-  spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
+   spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
+  document.querySelector(".elementor-widget-ucaddon_interactive_circle_infographic.overlay .elementor-widget-container::before").style.transform = rotateValue;
  });
 
 let graphic = document.querySelectorAll('.uc-circle');
@@ -25,8 +29,6 @@ graphic.forEach(el => el.addEventListener('click', () => {
   ineractiveDiv.classList.add('overlay');
   interactiveContainer.style.alignItems = 'start';
   interactiveContainer.style.textAlign = 'start';
-  let overlay = document.querySelector('.elementor-widget-ucaddon_interactive_circle_infographic .elementor-widget-container::before');
-  console.log(overlay, 'overlay');
 }));
 
 
