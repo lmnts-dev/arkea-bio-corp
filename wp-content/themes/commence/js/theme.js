f29f1af4f0142c9af76a848d46065edabe038f06
Jose Rios <jose@commence.studio>
2023-03-16 21:36:32 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -4,10 +4,11 @@ var scrollPosition = window.scrollY;
 let rotateValue = "rotate(" + scrollPosition + "deg)";
 let circle = document.querySelector(".circle");
 var lastScrollTop = 0;
+  let top2 = window.pageYOffset / 2; 
 
 window.addEventListener("scroll", () => {
   let top1 = window.pageYOffset / 5; 
-   let top2 = window.pageYOffset / 2; 
+  let top2 = window.pageYOffset / 2; 
    
   // spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
   spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
