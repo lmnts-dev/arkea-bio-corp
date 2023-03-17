1eea7bed4f5f356779195bad3fda37e5ae71f580
Jose Rios <jose@commence.studio>
2023-03-16 23:22:52 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -7,11 +7,9 @@ let circle = document.querySelector(".circle");
 var lastScrollTop = 0;
 
 window.addEventListener("scroll", () => {
-  let top1 = window.pageYOffset / 5; 
   let top2 = window.pageYOffset / 2; 
    
   // spiral.style.transform = 'rotateZ(' + top2 + 'deg)';
-  spiral2.style.transform = 'rotateZ(' + top2 + 'deg)';
      var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
    if (st > lastScrollTop) {
     circle.style.transform = 'rotateZ(' + top2 + 'deg)';
@@ -41,6 +39,8 @@ graphic.forEach(el => el.addEventListener('click', () => {
   interactiveContainer.style.textAlign = 'start';
 }));
 
+//SPIRALS ANIMATIONS
+
 let newSpirals = Array.from(spirals);
 let newSpirals2 = Array.from(spirals2);
 
@@ -54,31 +54,31 @@ window.addEventListener("scroll", () => {
     if (idx % 2 === 0) {
       spiral.style.transform = 'rotate(' + top3 + 'deg)';
     }
-    else if (idx % 3 == 0 ) {
+    else if (idx % 3 == 0) {
       spiral.style.transform = 'rotate(' + top4 + 'deg)';
     }
-    else if (idx % 5 == 0 ) {
+    else if (idx % 5 == 0) {
       spiral.style.transform = 'rotate(' + top5 + 'deg)';
     }
     else {
       spiral.style.transform = 'rotate(' + top6 + 'deg)';
     }
-  })
+  });
 
   newSpirals2.map((spiral, idx) => {
     if (idx % 2 === 0) {
       spiral.style.transform = 'rotate(' + top3 + 'deg)';
     }
-    else if (idx % 3 == 0 ) {
+    else if (idx % 3 == 0) {
       spiral.style.transform = 'rotate(' + top4 + 'deg)';
     }
-    else if (idx % 5 == 0 ) {
+    else if (idx % 5 == 0) {
       spiral.style.transform = 'rotate(' + top5 + 'deg)';
     }
     else {
       spiral.style.transform = 'rotate(' + top6 + 'deg)';
     }
-  })
+  });
 
   });
 
