15429696464b6211c0654131fe51b41a65271d5b
Jose Rios <jose@commence.studio>
2023-03-21 17:48:52 -0700

    :construction: Scroll

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -88,6 +88,16 @@ window.addEventListener("scroll", () => {
 
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 
 
 
