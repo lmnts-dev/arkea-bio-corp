fc1eee156a353fbdc06499f77404efcd29a5ab7c
Jose Rios <jose@commence.studio>
2023-06-10 20:54:33 -0700

    :construction: Update loop

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -35,13 +35,13 @@ graphic.forEach(el => el.addEventListener('click', () => {
   interactiveContainer.style.display = 'block';
   interactiveContainer.style.alignItems = 'start';
   interactiveContainer.style.textAlign = 'start';
-  close.map((el) => el.style.display = 'block');
+  close.forEach((el) => el.style.display = 'block');
 }));
 
 close.forEach(el => el.addEventListener("click", () => {
   interactiveContainer.style.display = 'none';
   infoBtns.style.display = 'none';
-  close.map((el) => el.style.display = 'none');
+  close.forEach((el) => el.style.display = 'none');
   ineractiveDiv.classList.remove('overlay');
 
 }));
