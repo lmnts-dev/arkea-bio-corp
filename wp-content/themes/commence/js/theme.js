3717cae802cd03fe95ed5bbbeff1c46cf908c200
Jose Rios <jose@commence.studio>
2023-06-13 16:29:45 -0700

    :construction: Buttons rework

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -38,9 +38,9 @@ graphic.forEach(el => el.addEventListener('click', () => {
   title.style.display = 'block';
 
   close.addEventListener("click", () => {
-    interactiveContainer.style.display = 'none';
-    infoBtns.style.display = 'none';
-    close.style.display = 'none';
+    interactiveContainer.style.opacity = '0';
+    infoBtns.style.display = '0';
+    close.style.display = '0';
     ineractiveDiv.classList.remove('overlay');
     startBtn.style.display = 'block'
   });
@@ -49,6 +49,7 @@ graphic.forEach(el => el.addEventListener('click', () => {
 startBtn.addEventListener("click", () => { 
   interactiveContainer.style.opacity = 1;
   infoBtns.style.opacity = 1;
+  startBtn.style.display = none;
 })
 
 
