a99508f0ed7831cf8c0e34c0295942612b37e102
Jose Rios <jose@commence.studio>
2023-06-14 11:32:20 -0700

    :construction: More

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -32,19 +32,20 @@ graphic.forEach(el => el.addEventListener('click', () => {
   infoBtns.style.display = 'block';
   // ineractiveDiv.classList.add('overlay');
   interactiveContainer.style.display = 'block';
-  interactiveContainer.style.opacity = '1';
+  // interactiveContainer.style.opacity = '1';
   interactiveContainer.style.alignItems = 'start';
   interactiveContainer.style.textAlign = 'start';
   let close = document.querySelector('.innerCircle .graphic-close');
   close.style.display = 'block';
   title.style.display = 'block';
+  startBtn.style.display = 'none'
 
   close.addEventListener("click", () => {
     interactiveContainer.style.opacity = '0';
     infoBtns.style.opacity = '0';
     close.style.display = '0';
     // ineractiveDiv.classList.remove('overlay');
-    startBtn.style.display = 'block'
+    startBtn.style.display = 'none'
   });
 }));
 
